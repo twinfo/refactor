54aaa2c44e353f5320c5ab065c2d1a0bc6df0455
twinfo <twinfo@sina.com>
2019-11-24 09:15:18 +0800

    this is refactor example

---
+++ src/chapter01_1/Proto01/Invoices.java
@@ -0,0 +1,28 @@
+package chapter01_1.Proto01;
+
+public class Invoices {
+    private String playID;
+    private Integer audience;
+
+    public Invoices(String playID, Integer audience) {
+        this.playID = playID;
+        this.audience = audience;
+    }
+
+    public String getPlayID() {
+        return playID;
+    }
+
+    public void setPlayID(String playID) {
+        this.playID = playID;
+    }
+
+    public Integer getAudience() {
+        return audience;
+    }
+
+    public void setAudience(Integer audience) {
+        this.audience = audience;
+    }
+}
+

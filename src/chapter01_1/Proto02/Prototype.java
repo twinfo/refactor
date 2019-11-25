b5664a66e08ed88042d6a2e97361a98a2ea6cf80
twinfo <twinfo@sina.com>
2019-11-25 10:22:10 +0800

    加入HTML打印功能
    Change-Id: I2ef3f36cbc7e76986b47b0d65d8a144b84468e59

--- src/chapter01_1/Proto02/Prototype.java
+++ src/chapter01_1/Proto02/Prototype.java
@@ -10,15 +10,20 @@ public class Prototype {
 	public static void main(String[] args) {
 		Prototype prototype = new Prototype();
 		System.out.println(prototype.statement());
+		System.out.println(prototype.htmlStatement());
 	}
 
 	//准备数据，传递给renderPlainText
 	public String statement() {
 		return renderPlainText(DataProcessBO.getInstance().createResultData());
 	}
+	
+	public String htmlStatement() {
+		return  renderHtmlstatement(DataProcessBO.getInstance().createResultData());
+	}
 
 	// 替换原有调用点
-	private String renderPlainText(ResultData data) {
+	private String renderPlainText(ResultData data) { // 减少接口依赖
 		String result = "Statement for " + data.getCustomer() + "\n";
 		for (Performances performances : data.getPerformances()) {
 			result += "  " + performances.getPlays().getName() + ": " + performances.getAmount() / 100 + "￥("
@@ -28,4 +33,19 @@ public class Prototype {
 		result += "You earned " + data.getVolumeCredits() + " credits\n";
 		return result;
 	}
+	
+	private String renderHtmlstatement(ResultData data) {
+        String result = "<h1>Statement for " + data.getCustomer() + "</h1>\n";
+        result += "<table>\n";
+        result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n";
+        for (Performances performances : data.getPerformances()) {
+            result += "<tr><td>" + performances.getPlays().getName() + "</td><td>" + performances.getPerf().getAudience() + "</td>";
+            result += "<td>" + performances.getAmount() / 100 + "￥</td></tr>\n";
+        }
+        result += "</table>\n";
+        result += "<p>Amount owed is <em>" + data.getTotalAmount()/ 100  + "￥</em></p>\n";
+        result += "<p>You earned <em>" + data.getVolumeCredits() + "</em> credits</p>\n";
+        return result;
+    }
+
 }
